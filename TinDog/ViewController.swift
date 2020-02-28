dd5c8bf429628f9b3edd528d48ab48eb3d0939f3
rafaelb@unicauca.edu.co <imac@Apple9.local>
2020-02-28 18:52:49 -0500

    First commit

---
+++ TinDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinDog
+//
+//  Created by Telematica on 2/28/20.
+//  Copyright © 2020 Telematica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
